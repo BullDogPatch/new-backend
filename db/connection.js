deeb6323073c0f8460b4fb398812f5fcf72d2d77
Craig Clayton <craig__clayton@outlook.com>
2024-02-21 14:54:09 +0000

    little change in db/connection.js

--- db/connection.js
+++ db/connection.js
@@ -10,14 +10,22 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE & DATABASE_URL not set");
 }
 
-const config =
-  ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
+// const config =
+//   ENV === 'production'
+//     ? {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       }
+//     : {};
+
+// ...
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 module.exports = new Pool(config);
